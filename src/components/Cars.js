1e5b7fbaaa231fb4d52b29181ad4fa63e710193e
Yann <yann.demaux86@gmail.com>
2023-12-10 11:08:35 +0100

    refacto

---
+++ src/components/Cars.js
@@ -0,0 +1,24 @@
+// import Wrapper from "./Wrapper";
+
+// const Car = ({ children, color }) => {
+//   let colorInfo = "";
+
+//   if (color) {
+//     colorInfo = color;
+//   } else {
+//     colorInfo = "Néant";
+//   }
+
+//   return (
+//     children && (
+//       <Wrapper>
+//         <p>Marque: {children} </p>
+//         <p>Couleur: {colorInfo}</p>
+//         {/*color ? <p>Couleur: {color}</p> : <p>Couleur: Néant</p>*/}
+//         {/*<p>Couleur :{color ? color : "Néant"}</p>*/}
+//       </Wrapper>
+//     )
+//   );
+// };
+
+// export default Car;
