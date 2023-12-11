1e4eb914920a12f067d121ea56999efa66272031
Yann <yann.demaux86@gmail.com>
2023-12-11 15:13:16 +0100

    add Welcome.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { Component } from "react";
 import Mycars from "./components/Mycars";
 import "./App.css";
+import Welcome from "./components/Welcome";
 
 class App extends Component {
-  state = {
-    titre: "Mon Catalogue Voitures",
-  };
-
   render() {
     return (
       <div className="App">
-        <Mycars title={this.state.titre} />
+        <Welcome />
+        <Mycars />
       </div>
     );
   }
--- src/components/Mycars.js
+++ src/components/Mycars.js
@@ -2,14 +2,54 @@ import { Component } from "react";
 import Car from "./Cars";
 
 class Mycars extends Component {
+  state = {
+    voitures: [
+      { name: "Ford", color: "red", year: 2022 },
+      { name: "Mercedes", color: "black", year: 2021 },
+      { name: "Peugeot", color: "green", year: 2018 },
+    ],
+    titre: "Mon Catalogue Voitures 2",
+  };
+
+  addTenYears = (e) => {
+    const updatedState = this.state.voitures.map((param) => {
+      return (param.year -= 10);
+    });
+
+    this.setState({ updatedState });
+  };
+
+  getAge = (year) => {
+    const now = new Date().getFullYear();
+    const age = now - year;
+    let frenchYearsStr = "";
+
+    if (age === 1) {
+      frenchYearsStr = "an";
+    } else if (age === 0) {
+      frenchYearsStr = "";
+    } else {
+      frenchYearsStr = "ans";
+    }
+    return `${age} ${frenchYearsStr}`;
+  };
+
   render() {
+    const year = new Date().getFullYear();
+
     return (
       <div>
-        <h1>{this.props.title}</h1>
+        <h1>{this.state.titre}</h1>
 
-        <Car color="red">Ford</Car>
-        <Car>Mercedes</Car>
-        <Car color="green"></Car>
+        <button onClick={this.addTenYears}> + 10 ans</button>
+
+        {this.state.voitures.map(({ name, color, year }, index) => {
+          return (
+            <div key={index}>
+              <Car name={name} color={color} year={this.getAge(year)} />
+            </div>
+          );
+        })}
       </div>
     );
   }
